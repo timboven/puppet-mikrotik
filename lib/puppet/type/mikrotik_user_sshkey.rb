972f1558fdeea0fbb099938223b513b059a8cfd6
brainkiller <brainkiller@gmail.com>
2019-01-15 22:22:20 +0100

    ssh_keys cannot be re-run

--- lib/puppet/type/mikrotik_user_sshkey.rb
+++ lib/puppet/type/mikrotik_user_sshkey.rb
@@ -1,7 +1,47 @@
 Puppet::Type.newtype(:mikrotik_user_sshkey) do
   apply_to_all
   
-  ensurable
+  #ensurable
+  ensurable do
+    defaultto :present
+
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    newvalue(:enabled) do
+      provider.create
+      provider.setState(:enabled)
+    end
+
+    newvalue(:disabled) do
+      provider.create
+      provider.setState(:disabled)
+    end
+
+    def retrieve
+      provider.getState
+    end
+
+    def insync?(is)
+      @should.each { |should|
+        case should
+          when :present
+            return (provider.getState != :absent)
+          when :absent
+            return (provider.getState == :absent)
+          when :enabled
+            return (provider.getState == :enabled)
+          when :disabled
+            return (provider.getState == :disabled)
+        end
+      }
+    end
+  end
 
   newparam(:user) do
     desc 'The user that the public key belongs to'
